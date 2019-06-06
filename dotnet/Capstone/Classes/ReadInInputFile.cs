b374f23e73fbfaca7afa265b29e7278743cc463a
Chad Campbell <campbellchadm@gmail.com>
2019-06-06 16:36:30 -0400

    new_class_inputFile_new_class_vendingMachine

---
+++ dotnet/Capstone/Classes/ReadInInputFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public static class ReadInInputFile
+    {
+       public static void InputInventory()
+        {
+            //string directory = Environment.CurrentDirectory;
+            string directory = @"C:\Users\Chad Campbell\Capstone\c - module - 1 - capstone - team - 6\dotnet\etc";
+            string filename = "vendingmachine.txt";
+            string fullPath = Path.Combine(directory, filename);
+
+            string slot = "";
+            string name = "";
+            string price = "";
+            string itemType = "";
+            try
+            {
+                using(StreamReader sr = new StreamReader(fullPath, false))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        // Read in a single line and 
+                        string line = sr.ReadLine();
+                        string[] input = line.Split("|");
+                        
+                 
+
+
+                    } //go to the next line until the end is reached
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Invalid File or Direcory.");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+    }
+}
