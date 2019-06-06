0e3ec32c18870e84b3049c759ec37df962fb595d
Josh Langer <joshlanger@gmail.com>
2019-06-06 14:22:02 -0400

    some classes added

---
+++ dotnet/Capstone/Classes/Beverage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Classes;
+
+namespace Capstone
+{
+    public class Beverage : Item
+    {
+        Beverage Cola = new Beverage();
+        Stack<int> ColaQuantity = new Stack<int>();
+    }
+}
---
+++ dotnet/Capstone/Classes/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Classes;
+
+namespace Capstone
+{
+    public class Item
+    {
+       protected string Name { get; set; }
+       protected decimal Price { get; set; }
+       Dictionary<string, Stack<Item>> VendingMachine = new Dictionary<string, Stack<Item>>();
+        public Item (string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+    }
+}
